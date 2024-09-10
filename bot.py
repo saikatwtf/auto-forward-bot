b8cafa0933b87f801e15bcc5e5beba1723a04b96
SaikatFTW <84172414+saikatwtf@users.noreply.github.com>
2024-09-10 17:16:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,16 +15,27 @@ TO_BOT_USERNAME = config("TO_BOT_USERNAME")  # Destination bot username
 
 # Create Pyrogram Client using StringSession
 app = Client(name=SESSION, session_string=SESSION, api_id=api_id, api_hash=api_hash)
+async def start_bot():
+    await app.start()
+    user = await app.get_me()
+    print(f"Logged in as : {user.first_name}")
+
+    await asyncio.Event().wait()
 
 # Function to forward messages from the source channel to the bot
+@app.on_message(filters.chat(source_channel))
 async def forward_messages(client, message):
-    try:
-        # Forward the message to the bot
-        await message.forward(TO_BOT_USERNAME)
-        print(f"Message forwarded: {message.message_id}")
-    except Exception as e:
-        print(f"Failed to forward message {message.message_id}: {e}")
-        except Exception as e:
+    for i in TO_BOT_USERNAME:
+        try:
+            await client.copy_message(
+                chat_id=i,
+                from_chat_id=message.chat.id,
+                message_id=message.id,
+                caption=message.caption,
+                caption_entities=message.caption_entities,
+                reply_markup=message.reply_markup
+            )
+ except Exception as e:
             print(e)
 
-app.run()
+app.run(start_bot())
